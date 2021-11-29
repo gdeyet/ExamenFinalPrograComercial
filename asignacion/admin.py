8baa9efce9f5dde5391bdf56fa7281ffaf9d2ff9
gdeyet <gdeyet@mesoamericana.edu.gt>
2021-11-29 11:32:39 -0600

    Config de admin

--- asignacion/admin.py
+++ asignacion/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from asignacion.models import Alumno, AlumnoAdmin, Curso, CursoAdmin
 
-# Register your models here.
+#Registramos nuestras clases principales.
+
+admin.site.register(Alumno, AlumnoAdmin)
+
+admin.site.register(Curso, CursoAdmin)
